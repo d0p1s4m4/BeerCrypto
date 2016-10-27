e289ef60f2950a321c3b27e93f3b267880ee469f
d0p1 <d0p1@yahoo.com>
2016-10-27 21:33:21 +0200

    Starting md5 in C (wip)

---
+++ src/md5.c
@@ -0,0 +1,16 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <d0p1@yahoo.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return 		d0p1
+ * ----------------------------------------------------------------------------
+ */
+
+typedef struct {
+} bc_md5_context_t;
+
+bc_md5_context_t	*bc_md5_init_ctx(void)
+{
+}
+
